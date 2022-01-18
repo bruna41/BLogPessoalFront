91edd18cfb68f838ca007153af5658027aee1523
Bruna <bruna41bergami@gmail.com>
2022-01-18 17:55:34 -0300

    @bruna41 [Adicionando entrar e cadastrar]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
+    CadastrarComponent,
     EntrarComponent,
-    CadastrarComponent
   ],
   imports: [
     BrowserModule,
