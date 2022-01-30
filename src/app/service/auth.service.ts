f62db213b1c22bcb5c3f818e0074238d208ceefd
Bruna <bruna41bergami@gmail.com>
2022-01-30 13:18:07 -0300

    @bruna41 [Atualizando put-user]

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://personalblogbru.herokuapp.com/usuarios/logar', userLogin)
+  }
+
+  cadastrar(usuario: User): Observable<User> {
+    return this.http.post<User>('https://personalblogbru.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  atualizar(usuario: User): Observable<User> {
+    return this.http.put<User>('https://personalblogbru.herokuapp.com/usuarios/atualizar', usuario)
+  }
+
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(`https://personalblogbru.herokuapp.com/usuarios/${id}`)
+  }
+
+  logado() {
+    let ok = false
+    if (environment.token != '')
+    ok = true
+    return ok
+  }
+}
